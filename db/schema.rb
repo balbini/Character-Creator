41ca4dc9f20dcc3acffbead71a936935e1d8c957
balbini <balbini91@gmail.com>
2017-10-25 17:29:22 -0700

    added languages to character model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171026002441) do
+ActiveRecord::Schema.define(version: 20171026002800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20171026002441) do
     t.integer "intelligence"
     t.integer "wisdom"
     t.integer "charisma"
+    t.string "languages"
   end
 
   create_table "users", force: :cascade do |t|
